f0cb0c7a402fb83cf0c7a62ffa4543844cf9ab4a
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2026-01-04 18:41:10 +0700

    update config for google images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  reactCompiler: true, // Biarkan settingan ini jika kamu pakai Next.js 15
+
+  // 👇 WAJIB TAMBAH INI (Agar avatar Google bisa muncul)
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
